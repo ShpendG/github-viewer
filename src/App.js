c9b04eaa65ce16963e03e47fe3099f6741553a2d
Shpend Gjela <shpendgjela77@gmail.com>
2023-11-24 10:49:32 +0100

    fixes + readme

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import { Form, Card, Image, Icon, Button } from 'semantic-ui-react';
 import logo from './logo.svg';
 import './App.css';
 
-
 function App() {
-  const [name, setName] = useState('');
+  const [name, setName] = useState('example-user');
   const [userName, setuserName] = useState('');
-  const [followers, setFollowers] = useState('');
-  const [following, setFollowing] = useState('');
-  const [repos, setRepos] = useState('');
+  const [url, setUrl] = useState('https://github.com/example');
+  const [followers, setFollowers] = useState('43');
+  const [following, setFollowing] = useState('0');
+  const [repos, setRepos] = useState('1');
   const [avatar, setAvatar] = useState('');
   const [userInput, setUserInput] = useState('');
 
-  useEffect(() => {
-    fetch("https://api.github.com/users/example")
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        setData(data);
-      });
-  }, []);
 
   const setData = ({
-    name, login, followers, following, public_repos, avatar_url
+    name, login, html_url, followers, following, public_repos, avatar_url
   }) => {
     setName(name);
     setuserName(login);
+    setUrl(html_url);
     setFollowers(followers);
     setFollowing(following);
     setRepos(public_repos);
@@ -45,13 +38,17 @@ function App() {
       })
   }
 
+  useLayoutEffect(() => {
+    document.body.style.backgroundColor = "#E4F8E3"
+  });
+
   return (
     <div>
       <div className='navbar'>GithubViewer</div>
       <div className='search'>
         <Form onSubmit={handleSubmit}>
           <Form.Group>
-            <Form.Input placeholder='Github User' name='github user' onChange={handleSearch} />
+            <Form.Input placeholder='Github Username' name='github user' onChange={handleSearch} />
             <Form.Button content='Search' />
           </Form.Group>
         </Form>
@@ -67,14 +64,20 @@ function App() {
           </Card.Content>
           <Card.Content extra>
             <a>
+              <Icon url='linkify' />
+              {url}
+            </a>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
               <Icon name='user' />
               {followers} Followers
             </a>
           </Card.Content>
           <Card.Content extra>
             <a>
-              <Icon name='user' />
-              {repos} Repos
+              <Icon name='code' />
+              {repos} Repo(s)
             </a>
           </Card.Content>
           <Card.Content extra>
